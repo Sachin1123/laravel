562fc4c8242269aff6839a1230e06d5b0bfe1d6e
Lalchand Sharma <sachinkumar.glocify@gmail.com>
2022-03-11 16:33:34 +0530

    setup

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use App\Models\Post;
+
+use Illuminate\Support\Facades\Validator;
+class UserController extends Controller
+{
+  
+    public function getUser(){
+        return new UserResource(User::all());
+    }
+
+    public function storePost(Request $request){
+       
+        $data = $request->all();
+
+        $validator = \Validator::make($request->all(), [
+            'title' => 'required',
+                'image' => 'required',
+                'gallery' => 'required',
+                'description' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+           return response()->json($validator->errors(), 422);
+        }
+        try {
+ 
+            if(!empty($data['gallery'])){
+                $imageName = time().'.'.$request->gallery->extension();  
+                $request->gallery->move(public_path('images'), $imageName);
+                $imgPath='/images/'.$imageName;
+            }
+            if(!empty($data['image'])){
+                $imageName = time().'.'.$request->image->extension();  
+               $request->image->move(public_path('images'), $imageName);
+               $GalleryPath='/images/'.$imageName;
+            }
+            $Post = new Post();
+            $Post->title = $data['title'];
+            $Post->description = $data['description'];
+            $Post->gallery = $GalleryPath;
+            $Post->image = $imgPath;
+
+            $Post->save();
+
+            $content = array(
+                'success' => true,
+                'data' => $Post,
+          
+            );
+
+            return response($content)->setStatusCode(200);
+        } catch (\Exception $e) {
+            $content = array(
+                'success' => false,
+                'data' => 'something went wrong.',
+                'message' => 'There was an error while processing your request: ' .
+                    $e->getMessage()
+            );
+            return response($content)->setStatusCode(500);
+        }
+        
+    }
+}
